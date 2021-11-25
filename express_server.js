deb1c7f1e300b80ddf95a2bdec517afe61e5913d
isissi <isissi_525@hotmail.com>
2021-11-24 17:38:33 -0700

    add generateRandomString()

--- express_server.js
+++ express_server.js
@@ -42,10 +42,24 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  let result = '';
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 6; i ++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
